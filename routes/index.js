4d7a467ce0a3693a1d9461ed11cb041f3174e644
Sebastian Grant <seb_grant@mac.com>
2014-08-30 14:03:30 +0100

    Styling for reports pages Include specificity in selectors list Sort by stylesheet line number or number of selectors

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ var css = require('css');
 var request = require('request');
 
 var parseSpec = function(specObj){
+
 	var _specObj = specObj.specificity.split(','),
 		parsedObj = {
 			raw  : specObj.specificity,
@@ -20,71 +21,94 @@ var parseSpec = function(specObj){
 	return parsedObj;
 };
 
-var parseSheet = function(sheetObj,url){
+var parseSelectors = function(selectors){
+	var _sels = [];
+	_.each(selectors, function(sel,s){
+		var specObj = specificity.calculate(sel)[0];
+		if(specObj && specObj.specificity){
+			_sels.push({
+				selector: sel,
+				specificity: parseSpec(specObj)
+			});
+		}
+
+	});
+	return _sels;
+}
+
+var parseSheet = function(sheetObj,url,sortOrder){
 
     var theCss = css.parse(sheetObj);
+
     if(theCss){
-	    var rules = theCss.stylesheet.rules;
+	    try {
+	        var rules = theCss.stylesheet.rules;
 
-        if(rules){
+		    if(rules){
 
-            var _rulesLength = rules.length,
-                _ruleSelectors = [],
-                _selectorsLength = 0,
-                _selectors = [],
-                _rules = [],
-                r = 0, s = 0,
-                _lastLessComment = "";
+			    var _rulesLength = rules.length,
+					    _ruleSelectors = [],
+					    _selectorsLength = 0,
+					    _selectors = [],
+					    _rules = [],
+					    r = 0, s = 0,
+					    _lastLessComment = "";
 
-            var buildRuleString = function(rule){
-                var _str = "";
-                _.each(rule.declarations, function(e,i){
-                    _str += e.property+":"+ e.value+"; ";
-                })
-                return _str;
-            }
+			    var buildRuleString = function(rule){
+				    var _str = "";
+				    _.each(rule.declarations, function(e,i){
+					    _str += e.property+":"+ e.value+"; ";
+				    })
+				    return _str;
+			    }
 
-            for (; r < _rulesLength; r++) {
-                if (rules[r] && rules[r].selectors){
+			    for (; r < _rulesLength; r++) {
+				    if (rules[r] && rules[r].selectors){
 
-                    _ruleSelectors = rules[r].selectors;
-                    _selectorsLength += rules[r].selectors.length;
-                    if(rules[r].declarations && "object" === typeof(rules[r].declarations)){
-                        var ruleObj = {
-                            rule:rules[r],
-                            declarations:rules[r].declarations,
-                            //ruleText: buildRuleString(rules[r]),
-                            selectorCount:_ruleSelectors.length,
-                            selectors:_ruleSelectors,
-                            lineNumber:rules[r].position.start.line,
-                            lessFile:_lastLessComment
-                        }
-                        _rules.push(ruleObj);
-                    }
-                    for(s in _ruleSelectors){
+					    _ruleSelectors = parseSelectors(rules[r].selectors);
+					    _selectorsLength += rules[r].selectors.length;
 
-                        var specObj = specificity.calculate(_ruleSelectors[s])[0];
-                        _selectors.push({
-                            selector:_ruleSelectors[s],
-                            specificity : parseSpec(specObj),
-                            ruleId:r
-                        });
-                    }
-                }
-            }
+					    if(rules[r].declarations[0] && rules[r].declarations[0].type==="comment"){
+						    if(rules[r].declarations[0].comment.indexOf('.less')>-1)
+						        _lastLessComment = rules[r].declarations[0].comment.replace(/-/g,'');
+					    }
 
-            _parsedObj = {
-                href : url,
-                rulesLength : _rulesLength,
-                rules : _.sortBy(_rules,'selectorCount').reverse(),
-                //rules : _.sortBy(_rules,'lineNumber'),
-                selectorsLength : _selectorsLength,
-                selectors : _.sortBy(_selectors,'specificity.value')
-            };
+					    if(rules[r].declarations && "object" === typeof(rules[r].declarations)){
+						    var ruleObj = {
+							    declarations:rules[r].declarations,
+							    selectorCount:_ruleSelectors.length,
+							    selectors:_ruleSelectors,
+							    lineNumber:rules[r].position.start.line,
+							    lessFile:_lastLessComment
+						    }
+						    _rules.push(ruleObj);
+					    }
+				    }
+			    }
 
-            return _parsedObj;
+			    _parsedObj = {
+				    href : url,
+				    rulesLength : _rulesLength,
+				    selectorsLength : _selectorsLength,
+				    selectors : _.sortBy(_selectors,'specificity.value')
+			    };
 
-        }
+			    if(sortOrder=='lineNumber'){
+				    _parsedObj.rules = _.sortBy(_rules,'lineNumber');
+			    } else {
+				    _parsedObj.rules = _.sortBy(_rules,'selectorCount').reverse();
+			    }
+
+			    return _parsedObj;
+
+		    }
+
+	    } catch(e){
+		    console.log("There was an error parsing "+url);
+		    console.log(e);
+	    }
+
+
     } else {
         console.log("The CSS could not be parsed");
     }
@@ -92,21 +116,32 @@ var parseSheet = function(sheetObj,url){
 };
 
 
-var createStyleSheetObjects = function(ssURLs,res){
+var createStyleSheetObjects = function(ssURLs,res,sortOrder){
 	var _sheets = ssURLs;
 	var _sheetCount = _sheets.length,
 		_sheetIDX = 0,
 		_ss = [];
 
-        _.each(ssURLs,function(url){
-            console.log("Create page for url "+url);
+        _.each(ssURLs,function(url,idx){
+	        if(url && url!=='null'){
 
-            request({uri:url,strictSSL:false},function(err,response,body){
-                _ss.push(parseSheet(body,url));
-                if(_ss.length===_sheetCount){
-                    res.render('report',{title:'CSSAudit',sheets:_ss});
-                }
-            });
+		        try {
+			        console.log("Sheet "+(idx+1)+" of "+_sheetCount);
+		            console.log("Getting stylesheet from: "+url);
+		            request({uri:url,strictSSL:false},function(err,response,body){
+		                _ss.push(parseSheet(body,url,sortOrder));
+			            if(_ss.length===_sheetCount){
+				            res.render('report',{title:'CSSAudit',sheets:_ss});
+			            }
+		            });
+
+		        } catch(e){
+			        console.log(e)
+		        }
+
+	        } else {
+		        _sheetCount--;
+	        }
 
         });
 
@@ -129,7 +164,13 @@ router.post('/specify',function(req,res){
 
 router.post('/report',function(req,res){
 
-	var auditUrl = req.body.siteurl;
+	var auditUrl = req.body.siteurl,
+		sortOrder = req.body.sortBy;
+
+	console.log("----|     CSSAUDIT     |----" )
+	console.log("Auditing: "+auditUrl);
+	console.log("Sorting by: "+sortOrder)
+	console.log("----------------------------" )
 
 	phantom.create("--web-security=no", "--ignore-ssl-errors=yes", { port: 12345 }, function (ph) {
 		ph.createPage(function (page) {
@@ -138,12 +179,14 @@ router.post('/report',function(req,res){
 				page.evaluate(function(){
                     var _ss = "", s=0;
                     for(;s<document.styleSheets.length;s++){
-                        _ss+=document.styleSheets.item(s)['href']+"|";
+	                    if(document.styleSheets.item(s)['href']){
+                            _ss+=document.styleSheets.item(s)['href']+"|";
+	                    }
                     }
                     return _ss.substring(0,_ss.length-1);
 				},function(result){
                     var ssURLs = result.split('|');
-                    var sheets = createStyleSheetObjects(ssURLs,res);
+                    var sheets = createStyleSheetObjects(ssURLs,res,sortOrder);
                     ph.exit();
 				});
 
