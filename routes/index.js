675386f974c5bacd1df08ff2ba77f930a8bc7ee5
Sebastian Grant <seb_grant@mac.com>
2014-09-02 10:59:42 +0100

    Make CSS utils a standalone module

--- routes/index.js
+++ routes/index.js
@@ -1,121 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var specificity = require('specificity');
-var _ = require('underscore');
-var css = require('css');
 var request = require('request');
 var jsdom = require('jsdom');
-
-var parseSpec = function(specObj){
-
-	var _specObj = specObj.specificity.split(','),
-		parsedObj = {
-			raw  : specObj.specificity,
-			thou : _specObj[0],
-			hund : _specObj[1],
-			tens : _specObj[2],
-			unit : _specObj[3],
-			value : (_specObj[0]*1000)+(_specObj[1]*100)+(_specObj[2]*10)+(parseInt(_specObj[3]))
-		};
-
-	return parsedObj;
-};
-
-var parseSelectors = function(selectors){
-	var _sels = [];
-	_.each(selectors, function(sel,s){
-		var specObj = specificity.calculate(sel)[0];
-		if(specObj && specObj.specificity){
-			_sels.push({
-				selector: sel,
-				specificity: parseSpec(specObj)
-			});
-		}
-
-	});
-	return _sels;
-}
-
-var parseSheet = function(sheetObj,url,sortOrder){
-
-    var theCss = css.parse(sheetObj);
-
-    if(theCss){
-	    try {
-	        var rules = theCss.stylesheet.rules;
-
-		    if(rules){
-
-			    var _rulesLength = rules.length,
-					    _ruleSelectors = [],
-					    _selectorsLength = 0,
-					    _selectors = [],
-					    _rules = [],
-					    r = 0, s = 0,
-					    _lastLessComment = "";
-
-			    var buildRuleString = function(rule){
-				    var _str = "";
-				    _.each(rule.declarations, function(e,i){
-					    _str += e.property+":"+ e.value+"; ";
-				    })
-				    return _str;
-			    }
-
-			    for (; r < _rulesLength; r++) {
-				    if (rules[r] && rules[r].selectors){
-
-					    _ruleSelectors = parseSelectors(rules[r].selectors);
-					    _selectorsLength += rules[r].selectors.length;
-
-					    if(rules[r].declarations[0] && rules[r].declarations[0].type==="comment"){
-						    if(rules[r].declarations[0].comment.indexOf('.less')>-1)
-						        _lastLessComment = rules[r].declarations[0].comment.replace(/-/g,'');
-					    }
-
-					    if(rules[r].declarations && "object" === typeof(rules[r].declarations)){
-						    var ruleObj = {
-							    declarations:rules[r].declarations,
-							    selectorCount:_ruleSelectors.length,
-							    selectors:_ruleSelectors,
-							    lineNumber:rules[r].position.start.line,
-							    lessFile:_lastLessComment
-						    }
-						    _rules.push(ruleObj);
-					    }
-				    }
-			    }
-
-			    var _parsedObj = {
-				    href : url,
-				    rulesLength : _rulesLength,
-				    selectorsLength : _selectorsLength,
-				    selectors : _.sortBy(_selectors,'specificity.value'),
-                    size      : Math.round((sheetObj.length / 1024))
-			    };
-
-			    if(sortOrder=='lineNumber'){
-				    _parsedObj.rules = _.sortBy(_rules,'lineNumber');
-			    } else {
-				    _parsedObj.rules = _.sortBy(_rules,'selectorCount').reverse();
-			    }
-
-			    return _parsedObj;
-
-		    }
-
-	    } catch(e){
-		    console.log("There was an error parsing "+url);
-		    console.log(e);
-	    }
-
-
-    } else {
-        console.log("The CSS could not be parsed");
-    }
-
-};
-
+var cssUtils = require('css-utils');
+var _ = require('underscore');
+var specificity = require('specificity');
 
 var createStyleSheetObjects = function(ssURLs,res,sortOrder){
 	var _sheets = ssURLs;
@@ -130,7 +19,7 @@ var createStyleSheetObjects = function(ssURLs,res,sortOrder){
 			        console.log("Sheet "+(idx+1)+" of "+_sheetCount);
 		            console.log("Getting stylesheet from: "+url);
 		            request({uri:url,strictSSL:false},function(err,response,body){
-                        _ss.push(parseSheet(body,url,sortOrder));
+                        _ss.push(cssUtils.parseSheet(body,url,sortOrder));
 			            if(_ss.length===_sheetCount){
 				            res.render('report',{title:'CSSAudit',sheets:_ss});
 			            }
@@ -158,7 +47,7 @@ router.post('/specify',function(req,res){
 
 	if(req.body.selector){
 		var specObj = specificity.calculate(req.body.selector)[0];
-		res.render('index', { specificity:parseSpec(specObj), selector:specObj.selector, title:'Specificty Tested on: '+req.body.selector } );
+		res.render('index', { specificity:cssUtils.parseSpecificity(specObj), selector:specObj.selector, title:'Specificty Tested on: '+req.body.selector } );
 	}
 
 });
